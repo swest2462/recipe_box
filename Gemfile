b2f71eb5c6efdbcb72b149b26870229217c87035
Aleksandr Vest <swest2462@gmail.com>
2015-12-13 14:39:18 +0300

    ingredients and directions in nested form

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'paperclip', '~> 4.3', '>= 4.3.2'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
 gem 'better_errors', '~> 2.1', '>= 2.1.1'
 gem 'cocoon', '~> 1.2', '>= 1.2.6'
+gem 'devise', '~> 3.5', '>= 3.5.3'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
-before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+  #before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @recipe = Recipe.all.order("created_at DESC")
@@ -9,13 +10,14 @@ before_action :find_recipe, only: [:show, :edit, :update, :destroy]
   end
 
   def new
-    @recipe = Recipe.new
+    @recipe = current_user.recipes.build
   end
 
   def create
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.build(recipe_params)
+
     if @recipe.save
-      redirect_to @recipe, notice: 'saved!'
+      redirect_to @recipe, notice: "Successfully created new recipe"
     else
       render 'new'
     end
@@ -26,7 +28,7 @@ before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
   def update
     if @recipe.update(recipe_params)
-      redirect_to @recipe, notice: 'updated!'
+      redirect_to @recipe
     else
       render 'edit'
     end
@@ -34,14 +36,13 @@ before_action :find_recipe, only: [:show, :edit, :update, :destroy]
 
   def destroy
     @recipe.destroy
-      redirect_to root_path, notice: 'deleted!'
+    redirect_to root_path, notice: "Successfully deleted recipe"
   end
-
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :image)
+    params.require(:recipe).permit(:title, :description, :image, ingredients_attributes: [:id, :name, :_destroy], directions_attributes: [:id, :step, :_destroy])
   end
 
   def find_recipe
